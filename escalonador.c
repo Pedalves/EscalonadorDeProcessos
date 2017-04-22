c2b0c25d2f0887e6f530d3818200dc0dce9ba348
root <root@localhost.localdomain>
2017-04-22 14:35:32 -0700

    interpretador adicionado

--- escalonador.c
+++ escalonador.c
@@ -1,15 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/shm.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #include "heap.h"
 
 #define MAX 30 
 
+int PRIORIDADE = 0, ROUND_ROBIN = 0, REAL_TIME = 0;	
+
+int shmProcesso; // segmento (shm) para disparo de novos processos no escalonador
+
+void adicionarProcesso(int signal) 
+{
+	char *novoProcesso; // Nome do novo processo (shm)
+	char nome[20];
+
+	int i, len, prioridade;
+	char msgCriacao[200];
+
+	novoProcesso = (char *) shmat(shmProcesso, 0, 0);
+
+	sscanf(novoProcesso, "%s", nome);
+
+	printf("->%s\n", nome);
+	fflush(stdout);
+
+	len = strlen(novoProcesso);
+
+	if(len > 3 && novoProcesso[len-2] == '=' && novoProcesso[len-3] == 'R') // Prioridade
+	{
+		//addPrioridade(nome, novoProcesso[len-1])
+	}
+	else if(len > 3 && (novoProcesso[len-2] == 'D' || novoProcesso[len-3] == '=')) // Real Time
+	{
+		//kill(pidEscalonador, SIGUSR2);
+	}
+	else // Round Robin
+	{
+		//kill(pidEscalonador, SIGALRM);
+	}
+
+	shmdt(novoProcesso);
+}
+
 pid_t addPrioridade(char* prog, int prioridade)
 {
-	char* vet3[] = {prog, NULL};	
+	char* vet[] = {prog, NULL};	
 	pid_t pid;
 
 	if ((pid = fork()) < 0) 
@@ -17,9 +59,9 @@ pid_t addPrioridade(char* prog, int prioridade)
 		fprintf(stderr, "Erro ao criar filho\n"); 
 		exit(-1); 
 	} 
-	if (pid1 == 0)
+	if (pid == 0)
 	{
-		execv("prog1", vet1);
+		execv(prog, vet);
 		exit(1);
 	}
 	kill(pid, SIGSTOP);
@@ -27,12 +69,13 @@ pid_t addPrioridade(char* prog, int prioridade)
 	return pid;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-	int PRIORIDADE = 0, ROUND-ROBIN = 0, REAL-TIME = 0;	
+	printf("Escalonador iniciado\n");
+	fflush(stdout);
 
 	// REAL-TIME
-	pid_t pidPrioridade = -1
+	pid_t pidRealTime = -1;
 	
 	// PRIORIDADE
 	Heap* heapPrioridade = heap_cria(MAX);
@@ -45,11 +88,15 @@ int main()
 	int i;
 
 	for(i = 0; i<MAX; i++)
-		roudRobin[i] = -1;	
+		roundRobin[i] = -1;	
 
-	while(true)
+
+	shmProcesso = atoi(argv[1]);
+	signal(SIGUSR1, adicionarProcesso);
+
+	while(1)
 	{
-		if(REAL-TIME)
+		if(REAL_TIME)
 		{
 
 		}
@@ -57,7 +104,7 @@ int main()
 		{
 			heap_remove(heapPrioridade);
 		}
-		else if(ROUND-ROBIN)
+		else if(ROUND_ROBIN)
 		{
 			kill(roundRobin[currRR], SIGCONT);
 			sleep(0.5);
---
+++ interpretador.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+
+int fileExec, fileIN, fileOUT; // arquivos de execução, entrada e saída
+int pidEscalonador = -1; // PID do escalonador
+
+int segmentoNomePrograma; // segmento para disparo de novos processos no escalonador
+
+int openFile(char *name, int type) 
+{
+	int file;
+	file = open(name, type, S_IRWXU|S_IRWXG|S_IRWXO);
+	if(file == -1) {
+		printf("[ERRO] Nao foi possivel abrir o arquivo %s", name);
+		fflush(stdout);
+		exit(-1);
+	}
+	return file;
+}
+
+void endHandler(int signal) 
+{
+	if(pidEscalonador != -1)
+	{
+		kill(SIGINT, pidEscalonador); // Encerra escalonador
+	}
+
+	close(fileExec); // Fecha arquivo de execução
+	close(fileIN); // Fecha arquivo de entrada
+	close(fileOUT); // Fecha arquivo de saída
+
+	shmctl(segmentoNomePrograma, IPC_RMID, 0); // Libera memória compartilhada
+
+	exit(0);
+}
+
+void criaProcessoEscalonador(int fd[2]) {
+	char argNomePrograma[11];
+
+	segmentoNomePrograma = shmget(IPC_PRIVATE, sizeof(char) * 1024, IPC_CREAT|IPC_EXCL|S_IRUSR|S_IWUSR);
+
+	pidEscalonador = fork(); // cria processo para o escalonador
+	if(pidEscalonador == 0) // escalonador
+	{
+		close(fd[0]); // fecha pipe de leitura
+		dup2(fd[1], 1); // redireciona stdout para pipe de escrita
+
+		snprintf(argNomePrograma, 11, "%d", segmentoNomePrograma);
+		char * execEscalonador[] = {"./escalonador", argNomePrograma, NULL};
+		execve("./escalonador", execEscalonador, 0);
+		exit(0);
+	}
+	else
+	{
+		dup2(fd[0], 0); // redireciona stdin para pipe de leitura
+	}
+
+	sleep(1);
+}
+
+void adicionaProcessos() {
+	char buffer[2048];
+	int cnt = 0, i, len;
+	char *nomeProcesso;
+
+	nomeProcesso = (char *) shmat(segmentoNomePrograma, 0, 0);
+
+	while(read(fileExec,&buffer[cnt],1) != 0) {
+		if(buffer[cnt] == '\n' && cnt > 1) {
+			buffer[cnt] = '\0';
+			strcpy(nomeProcesso, &buffer[5]);
+			printf("Novo processo %s\n", nomeProcesso);
+			fflush(stdout);
+
+			len = strlen(nomeProcesso);
+
+			kill(pidEscalonador, SIGUSR1);
+
+			sleep(1);
+			buffer[0] = '\0';
+			cnt = 0;
+		}
+	cnt++;
+	}
+
+	shmdt(nomeProcesso);
+}
+
+int main(void) {
+	int fd[2]; // pipe
+
+	fileExec = openFile("exec.txt", O_RDONLY); // Abre arquivo para leitura
+	fileIN = openFile("entrada.txt", O_RDONLY); // Abre arquivo para leitura
+	fileOUT = openFile("saida.txt", O_CREAT|O_RDWR|O_TRUNC); // Abre arquivo para escrita
+
+	signal(SIGINT, endHandler);
+
+	if(pipe(fd) < 0) { // abre pipe
+		printf("Nao foi possivel abrir o pipe\n");
+		fflush(stdout);
+		exit(-1);
+	}
+
+	if(dup2(fileIN, 0) == -1) { // redireciona stdin para arquivo de entrada
+		printf("Nao foi possivel redirecionar o stdin para arquivo de entrada\n");
+		fflush(stdout);
+		exit(-1);
+	}
+
+	if(dup2(fileOUT, 1) == -1) { // redireciona stdout para arquivo de saída
+		printf("[ERRO] Nao foi possivel redirecionar o stdout para arquivo de saida\n");
+		fflush(stdout);
+		exit(-1);
+	}
+	if(dup2(fileOUT, 2) == -1) { // redireciona stderr para arquivo de saída
+		printf("[ERRO] Nao foi possivel redirecionar o stderr para arquivo de saida\n");
+		fflush(stdout);
+		exit(-1);
+	}
+
+	close(fd[1]); // fecha pipe de escrita
+
+	criaProcessoEscalonador(fd);
+	adicionaProcessos();
+	while(1) sleep(1);
+	waitpid(pidEscalonador, NULL, 0);
+
+	close(fileExec);
+	close(fileIN);
+	close(fileOUT);
+
+	shmctl(segmentoNomePrograma, IPC_RMID, 0);
+
+	return 0;
+}
